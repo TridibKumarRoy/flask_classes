4a5d3dbb0fc73137aaa89d8ed2ce969550453ed8
TridibKumarRoy <118563573+TridibKumarRoy@users.noreply.github.com>
2023-09-23 20:25:33 +0530

    2ND CLASS

--- class 1/flsk1.py
+++ class 1/flsk1.py
@@ -1,5 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/flask_class_2023'
+db = SQLAlchemy(app)
+
+class Contact(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(30))
+    phone = db.Column(db.String(10))
+    message = db.Column(db.String(100))
+    date = db.Column(db.String(120))
+    
 
 @app.route("/")
 def home():
@@ -9,8 +22,17 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route("/contact")
+@app.route("/contact", methods =['GET','POST'])
 def contact():
+    if(request.method == 'POST'):
+        name = request.form.get('name')
+        email_add = request.form.get('email')
+        ph_no = request.form.get('phone')
+        msg = request.form.get('message')
+        
+        entry = Contact(name = name, email=email_add,phone=ph_no,message=msg)
+        db.session.add(entry)
+        db.session.commit()
     return render_template('contact.html')
 
 @app.route("/post")
